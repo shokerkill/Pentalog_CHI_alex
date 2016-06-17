1b86de08a3abce5597c6a006aee596fae0383366
Alexandr VIDIS <avidis@users-MacBook-Pro-2.local>
2016-06-17 17:26:39 +0300

    garbage collection

--- app/views.py
+++ app/views.py
@@ -16,13 +16,8 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    # Here we use a class of some kind to represent and validate our
-    # client-side form data. For example, WTForms is a library that will
-    # handle this for us, and we use a custom LoginForm to validate.
     form = LoginForm()
     if form.validate_on_submit():
-        # Login and validate the user.
-        # user should be an instance of your `User` class
         user = form.login_name.data
         user = User.query.filter(User.nickname == user).first()
         if user:
